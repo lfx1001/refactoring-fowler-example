e9375854d85adfdddaf3765bf25977949d8dc1bb
L.Fernandez <lfx1001@alu.ubu.es>
2019-05-16 18:27:28 +0200

    Rename method parameter

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -60,24 +60,24 @@ public class Customer {
 	}
 
 	/**
-	 * @param each
+	 * @param aRental
 	 * @return
 	 */
-	private double amountFor(Rental each) {
+	private double amountFor(Rental aRental) {
 		float result = 0;
-		switch (each.getMovie().getPriceCode()) {
+		switch (aRental.getMovie().getPriceCode()) {
 		case Movie.REGULAR:
 			result += 2;
-			if (each.getDaysRented() > 2)
-				result += (each.getDaysRented() - 2) * 1.5;
+			if (aRental.getDaysRented() > 2)
+				result += (aRental.getDaysRented() - 2) * 1.5;
 			break;
 		case Movie.NEW_RELEASE:
-			result += each.getDaysRented() * 3;
+			result += aRental.getDaysRented() * 3;
 			break;
 		case Movie.CHILDRENS:
 			result += 1.5;
-			if (each.getDaysRented() > 3)
-				result += (each.getDaysRented() - 3) * 1.5;
+			if (aRental.getDaysRented() > 3)
+				result += (aRental.getDaysRented() - 3) * 1.5;
 			break;
 		}
 		return result;
