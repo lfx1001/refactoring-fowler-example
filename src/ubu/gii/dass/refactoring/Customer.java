9e6fd8484bf9f307d011ff44c56a971e7e2d5236
L.Fernandez <lfx1001@alu.ubu.es>
2019-05-17 23:37:50 +0200

    #1 replace temp whit query getTotalCharge()

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -31,7 +31,6 @@ public class Customer {
 	};
 
 	public String statement() {
-		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Iterator<Rental> rentals = _rentals.iterator();
 		String result = "Rental Record for " + getName() + "\n";
@@ -46,12 +45,21 @@ public class Customer {
 			// show figures for this rental
 			result += "\t" + rental.getMovie().getTitle() + "\t"
 					+ String.valueOf(rentalAmount) + "\n";
-			totalAmount += rentalAmount;
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
+		return result;
+	}
+	
+	private double getTotalCharge() {
+		double result = 0;
+		Iterator<Rental> rentals = _rentals.iterator();
+		while (rentals.hasNext()) {
+			Rental rental = rentals.next();
+			result += rental.getCharge();
+		}
 		return result;
 	}
 }
